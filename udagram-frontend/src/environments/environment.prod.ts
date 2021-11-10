5c1d5b99367d8ce4ae508942dad60f0105f48bc1
Osi <osiking07@gmail.com>
2021-11-10 21:53:16 +0100

    Added deployment folder

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aaabd38e1820c403a87a7c9cd785e767-494649747.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
